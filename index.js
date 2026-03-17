e9bd743caf9abb0d6b66ddfa1913e077a81de631
santiago-abuawad <mbitakrec@gmail.com>
2026-03-17 18:58:40 -0400

    seccion about,experience,blog,projects,contact agregadas form en el contact

--- index.js
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Santiago Abuawad</title>
-    <link rel="stylesheet" href="index.css">
-</head>
