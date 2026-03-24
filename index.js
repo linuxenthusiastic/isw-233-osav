947eb4ef377582bfa06439c8385e8071deb083b0
santiago-abuawad <mbitakrec@gmail.com>
2026-03-24 15:56:42 -0400

    nuevos estilos terminados y modificacion en js de la nav

--- index.js
+++ index.js
@@ -28,3 +28,15 @@ const btnProjects = document.querySelector("#btnProjects");
 btnProjects.addEventListener("click", () => {
     navegarA("projects");
 })
+
+
+links.forEach(l => {
+    const text = l.textContent;
+    l.addEventListener("mouseover", () => {
+        l.textContent = text.toUpperCase();
+    })
+
+    l.addEventListener("mouseout", () => {
+        l.textContent = text;
+    })
+})
