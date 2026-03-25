54421c16a6b3f5820d360ee67e9891a13a1841d8
santiago-abuawad <mbitakrec@gmail.com>
2026-03-24 23:41:59 -0400

    fixed bugs agregamos distintos tipos de observer y agregamos nuevo css a blog

--- index.js
+++ index.js
@@ -1,63 +1,75 @@
 const links = document.querySelectorAll("nav a");
 const secciones = document.querySelectorAll(".seccion");
-
-function navegarA(id){
-    secciones.forEach(seccion => {
-        seccion.classList.remove("activa");
-        seccion.classList.remove("visible");
-    })
-
-    const destino = document.querySelector(`#${id}`);
-    if(destino) {
-        destino.classList.add("activa");
-        setTimeout(() => {
-            destino.classList.add("visible");
-        },50);
-    }
-}
-
-links.forEach(link => {
-    link.addEventListener("click", (evento) => {
-        evento.preventDefault();
-
-        const href = link.getAttribute("href");
-        const id = href.replace("#", "");
-
-        navegarA(id);
-    });
-});
-
-
-const btnProjects = document.querySelector("#btnProjects");
-btnProjects.addEventListener("click", () => {
-    navegarA("projects");
-})
-
-const btnContact = document.querySelector('.btn-secundario')
-btnContact.addEventListener("click" , () => {
-    navegarA("contact")
-})
+const blogGrid = document.querySelector("#blogGrid");
+const contadorPost = document.querySelector("#contadorPost");
+const btnAgregarPost = document.querySelector("#btnAgregarPost")
 
 links.forEach(l => {
     const text = l.textContent;
     l.addEventListener("mouseover", () => {
         l.textContent = text.toUpperCase();
     })
-
     l.addEventListener("mouseout", () => {
         l.textContent = text;
     })
 })
 
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting){
+        if(entry.isIntersecting) {
             entry.target.classList.add("visible");
+        } else {
+            entry.target.classList.remove("visible");
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+secciones.forEach(seccion => {
+    observer.observe(seccion);
+});
+
+const mutationObserver = new MutationObserver((mutations) => {
+    mutations.forEach(mutation => {
+        if(mutation.addedNodes.length > 0){
+            const total = blogGrid.children.length;
+            contadorPost.textContent = `Posts ${total}`
         }
     })
 })
 
-secciones.forEach(seccion => {
-    observer.observe(seccion);
+mutationObserver.observe(blogGrid,{
+    childList: true
+})
+
+const lazyObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            const img = entry.target;
+            img.src = img.dataset.src;
+            img.classList.remove("lazy");
+            lazyObserver.unobserve(img);
+        }
+    })
+})
+
+let numeroPost = 0;
+btnAgregarPost.addEventListener("click", () => {
+    numeroPost++;
+    const article = document.createElement("article");
+
+    article.innerHTML = `
+    <h3>Blog #${numeroPost}</h3>
+    <p>Hoy comi pescado</p>
+    <img data-src="https://picsum.photos/400/200?random=${numeroPost}" 
+    alt="imagen del post"
+    class="lazy"
+    style="width:100%; height:200px; background:#222;"/>
+    `
+
+    blogGrid.appendChild(article);
+
+    const nuevaImg = article.querySelector(".lazy");
+    lazyObserver.observe(nuevaImg);
 })
